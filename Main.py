4c9b003fa8477fac6d0369b3f2624443f450443e
tany3450 <tanayozefe@gmail.com>
2016-08-23 14:03:42 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import gradleParser as gr
+from androGuard.apk import APK
+import re
+from ConfigParser import SafeConfigParser
+
+parser = SafeConfigParser()
+
+
+def apk_SIZE(size):
+    if size < 15728640:
+        print "APK2 - Success. APK size is less than 15 MB."
+    else:
+        print "APK2 - Fail. APK size is more than 15 MB."
+
+
+def min_SDK(min_sdk_version):
+    if min_sdk_version == parser.get('B5', 'minSdkVersion'):
+        print "B5 - Success. MinSDKVersion is set to 16."
+    else:
+        print "B5 - Fail. MinSDKVersion is not set to 16."
+
+
+def target_SDK(target_sdk_version):
+    if target_sdk_version == parser.get('B6', 'targetSdkVersion'):
+        print "B6 - Success. TargetSDKVersion is set to 24."
+    else:
+        print "B6 - Fail. TargetSDKVersion is not set to 24."
+
+
+def package_NAME(package_name):
+    if package_name[:17] == "com.monitise.mea.":
+        print "B4 - Success. ApplicationId respects com.monitise.mea.<product> convention."
+    else:
+        print "B4 - Fail. ApplicationId does not respect com.monitise.mea.<product> convention."
+
+
+def manifest_AllowBackup(application):
+    try:
+        if application.attributes['android:allowBackup'].value == parser.get('SEC1', 'allowBackup'):
+            print "SEC1 - Success. android:allowBackup is set to false."
+        else:
+            print "SEC1 - Fail. android:allowBackup is set to true."
+    except:
+        print "SEC1 - Fail. android:allowBackup is not set."
+
+
+def manifest_InstallLocation(application):
+    try:
+        if application.attributes['android:installLocation'].value != "externalOnly":
+            print "MAN5 - Success. android:installLocation is not set to externalOnly."
+        else:
+            print "MAN5 - Fail. android:installLocation is set to externalOnly."
+    except:
+        print "MAN5 - Success. android:installLocation is not set."
+
+
+def dependencyChecker(dependencies):
+    errorGoogleAPI = False
+    errorVersion = False
+    for dependency in dependencies:
+        if (len(dependency) == 1):
+            if (re.search('[+]+', dependency[0]) != None):
+                errorVersion = True
+                # print "B8 - " + dependency
+            test=dependency[0][:37]
+            if ("com.google.android.gms:play-services:" == dependency[0][:37]):
+                errorGoogleAPI = True
+    if (errorGoogleAPI == False):
+        print "B7 - Success. Google Play Services API is included as separate dependencies."
+    else:
+        print "B7 - Fail. Google Play Services API is not included as separate dependencies."
+    if errorVersion == False:
+        print "B8 - Success. Each dependency injected has a specific version specified."
+    else:
+        print "B8 - Fail. Some dependencies injected have not specified a version."
+
+
+def versionName(version_name):
+    if (version_name > parser.get('MAN2', 'previousVersionName')):
+        print "MAN2 - Success. android:versionName attribute is updated."
+    else:
+        print "MAN2 - Fail. android:versionName attribute is not updated."
+
+    if (len(version_name.split(".")) == 4):
+        print "MAN3 - Success. android:versionName follows <major>.<minor>.<patch>.<buildNumber> convention."
+    else:
+        print "MAN3 - Fail. android:versionName does not follow <major>.<minor>.<patch>.<buildNumber> convention."
+
+
+def proguardChecker(release_build):
+    try:
+        if (release_build["minifyEnabled"][0][0] == "true" and release_build["shrinkResources"][0][0] == "true"):
+            print "PRG2 - Success. minifyEnabled and shrinkResoureces are set to true for release build type."
+        else:
+            print "PRG2 - Fail. minifyEnabled or/and shrinkResoureces are not set to true for release build type."
+    except:
+        print "PRG2 - Error. minifyEnabled or/and shrinkResources could not be found."
+
+    allIncluded = True
+    proguardFilesGradle = release_build["proguardFiles"]
+    proguardFilesConfig = parser.get('PRG3', 'proguardList').split()
+    for proguardFileConfig in proguardFilesConfig:
+        if not (proguardFileConfig in proguardFilesGradle[0]):
+            allIncluded = False
+    if (allIncluded == True):
+        print "PRG3 - Success. proguard configurations files are added through proguardFiles property in the release build type."
+    else:
+        print "PRG3 - Fail. not all proguard configurations files are added through proguardFiles property in the release build type."
+
+
+def debuggableChecker(release_build):
+    try:
+        test = release_build["debuggable"][0]
+        if (release_build["debuggable"][0][0] != 'true'):
+            print "B9 - Success. Release build type in gradle build file doesn't have debuggable set to true."
+        else:
+            print "B9 - Fail. Release build type in gradle build file has debuggable set to true."
+    except:
+        print "B9 - Success. Release build type in gradle build file doesn't have debuggable attribute."
+
+
+def signingConfigs(signingConfigs_release):
+    try:
+        if (signingConfigs_release["storeFile"][0] != None):
+            print "SIGN2 - Success. Release keystore is included in source control."
+    except:
+        print "SIGN2 - Fail. Release keystore is not included in source control."
+    try:
+        if (signingConfigs_release["storePassword"][0] != None
+            and signingConfigs_release["keyAlias"][0] != None
+            and signingConfigs_release["keyPassword"][0] != None):
+            print "SIGN4 - Success. StorePassword, keyAlias, keyPassword are set in the build.gradle file for release signing config."
+    except:
+        print "SIGN4 - Fail. storePassword, keyAlias, keyPassword are not set in the build.gradle file for release signing config."
+
+
+def exportedCheck(services, activities, receivers):
+    errorCheck = False
+    for service in services:
+        try:
+            if service.getElementsByTagName('intent-filter')[0] != None:
+                try:
+                    if service.attributes['android:exported'].value != "false":
+                        errorCheck = True
+                except:
+                    errorCheck = True
+        except:
+            None
+    for activity in activities:
+        try:
+            if activity.getElementsByTagName('intent-filter')[0] != None:
+                try:
+                    if activity.attributes['android:exported'].value != "false":
+                        errorCheck = True
+                except:
+                    errorCheck = True
+        except:
+            None
+    for receiver in receivers:
+        try:
+            if receiver.getElementsByTagName('intent-filter')[0] != None:
+                try:
+                    if receiver.attributes['android:exported'].value != "false":
+                        errorCheck = True
+                except:
+                    errorCheck = True
+        except:
+            None
+    if errorCheck != True:
+        print "SEC4 - Success. Android:exported is set to false for all services, activities and broadcast receivers containing intent filters."
+    else:
+        print "SEC4 - Fail. Android:exported is not set to false for some/all services, activities and broadcast receivers containing intent filters."
+
+
+def versionCode(defaultConfig):
+    if (defaultConfig["versionCode"][0][0] > parser.get('MAN1', 'previousVersionCode')):
+        print "MAN1 - Success. android:versionCode is incremented."
+    else:
+        print "MAN1 - Fail. android:versionCode is not incremented."
+
+
+def hardwareFeatures(features, featuresIsRequired):
+    i = 0
+    error = False
+    for feature in features:
+        if "hardware" in feature:
+            if featuresIsRequired[i] != "false":
+                error = True
+        i = i + 1
+    if error != True:
+        print "PERM3 - Success. android:required is set to false for hardware features."
+    else:
+        print "PERM3 - Fail. android:required is not set to false for hardware features."
+
+
+def main():
+    parser.read('config.ini')
+    myapk = APK("/users/ozefet/AndroidStudioProjects/UdacityMovies/app/build/outputs/apk/app-debug.apk")
+    manifestXML = myapk.get_android_manifest_xml()
+    gradleResult = gr.GradleParser().parse(False)
+    apk_SIZE(myapk.file_size)
+    package_NAME(myapk.package)
+    min_SDK(myapk.get_min_sdk_version())
+    target_SDK(myapk.get_target_sdk_version())
+    dependencyChecker(gradleResult["dependencies"]["compile"])
+    debuggableChecker(gradleResult["android"]["buildTypes"][0]["release"][0])
+    manifest_AllowBackup(manifestXML.getElementsByTagName('application')[0])
+    versionName(manifestXML.getElementsByTagName('manifest')[0].attributes['android:versionName'].value)
+    manifest_InstallLocation(manifestXML.getElementsByTagName('application')[0])
+    proguardChecker(gradleResult["android"]["buildTypes"][0]["release"][0])
+    signingConfigs(gradleResult["android"]["signingConfigs"][0]["release"][0])
+    versionCode(gradleResult["android"]["defaultConfig"][0])
+    exportedCheck(
+        manifestXML.getElementsByTagName('manifest')[0].getElementsByTagName('application')[0].getElementsByTagName(
+            'service')
+        , manifestXML.getElementsByTagName('manifest')[0].getElementsByTagName('application')[0].getElementsByTagName(
+            'activity')
+        , manifestXML.getElementsByTagName('manifest')[0].getElementsByTagName('application')[0].getElementsByTagName(
+            'receiver'))
+    hardwareFeatures(myapk.features, myapk.featuresIsRequired)
+
+
+if __name__ == "__main__":
+    main()
---
+++ gradleParser.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env pyth
+
+from collections import defaultdict
+import tokenize
+import json
+
+
+class GradleParser(object):
+    """
+    This class will parse standard build gradle file and create a nested dictionary
+    """
+    dictionary_list = ["outside"]
+    gradle_dict = dict()
+    tokens_initial = list()
+    token_list = list()
+
+    def __init__(self):
+        self.file = open("/users/ozefet/Downloads/build.gradle", "r")
+
+        def handle_token(type, token, (srow, scol), (erow, ecol), line):
+            self.tokens_initial.append((tokenize.tok_name[type], repr(token).strip("\"\'")))
+
+        tokenize.tokenize(self.file.readline, handle_token)
+        # Delete rogue newlines between dict names and {'s
+        self.token_list.append(self.tokens_initial[0])
+        for i in range(1, len(self.tokens_initial) - 1):
+            if not (self.tokens_initial[i - 1][0] == "NAME" and self.tokens_initial[i + 1][1] == "{"):
+                self.token_list.append(self.tokens_initial[i])
+        self.token_list.reverse()
+
+    def parse(self,will_xml):
+        """
+        This method parses the given token_list object and returns either xml or nested dictionary
+        @param will_xml: return the result as xml if given true
+        @return: nested dictionary or xml object
+        """
+        # first dictionary initialization
+        self.gradle_dict[self.dictionary_list[0]] = defaultdict(list)
+        while not (len(self.token_list) == 0):
+            parse_line = []
+            while True:
+                parse_line.append(self.token_list.pop())
+                if parse_line[-1][0] == 'NEWLINE' or parse_line[-1][0] == 'NL' or len(self.token_list) == 0:
+                    break;
+            # containment checks
+            if len(parse_line) == 1 and self.checkElements(parse_line, ['\\n']):
+                pass
+            # special case, mavens
+            elif self.checkElements(parse_line, ["{"]) and self.checkElements(parse_line, ["}"]):
+
+                new_dict = parse_line[0][1]
+                parent_node = self.dictionary_list[-1]
+
+                if not (new_dict in self.gradle_dict[parent_node].keys()):
+
+                    self.gradle_dict[parent_node][new_dict] = defaultdict(list)
+
+                element_list = self.purifyElements(parse_line, ["{", "}"])
+                self.gradle_dict[parent_node][new_dict][element_list[1]].append(element_list[2:-1])
+            # New nested dictionary node opening
+            elif self.checkElements(parse_line, ["{"]):
+                # Special case, compiles
+                if self.checkElements(parse_line, ["(", ")"]):
+                    element_list = self.purifyElements(parse_line, ["(", ")"])
+                    new_dict = element_list[1]
+                    self.dictionary_list.append(new_dict)
+                    self.gradle_dict[new_dict] = defaultdict(list)
+                else:
+                    new_dict = parse_line[0][1]
+                    self.dictionary_list.append(new_dict)
+                    self.gradle_dict[new_dict] = defaultdict(list)
+            # Nested dictionary element addition
+            elif not (self.checkElements(parse_line, ["{", "}"])):
+
+                current_node = self.gradle_dict[self.dictionary_list[-1]]
+                element_list = self.purifyElements(parse_line, ["=", ":", ",", ";", "file", "(", ")"])
+
+                if self.checkElements(element_list, ["."]):
+
+                    self.purifyElements(element_list, ["."])
+                    joined_element = ".".join(element_list[1:-1])
+                    current_node[element_list[0]].append(joined_element)
+
+                elif len(element_list) == 2:
+                    current_node[element_list[0]].append(element_list[0])
+
+                else:
+                    element_list = self.purifyElements(element_list,['[',']',':',','])
+                    current_node[element_list[0]].append(element_list[1:-1])
+            # Dictionary closing
+            elif self.checkElements(parse_line, ["}"]):
+
+                current_node = self.dictionary_list.pop()
+
+                if len(self.dictionary_list) > 1:
+
+                    parent_node = self.dictionary_list[-1]
+                    self.gradle_dict[parent_node][current_node].append(self.gradle_dict[current_node])
+                    del self.gradle_dict[current_node]
+        # Replace root project version if any exists
+        if "ext" in self.gradle_dict.keys():
+            self.parse_versions()
+
+        if(will_xml):
+            return json.dumps(self.gradle_dict,sort_keys=True,indent=4)
+        else:
+            return self.gradle_dict
+
+    def parse_versions(self):
+        """
+        extract versions from the ext block if exists
+        @return:
+        """
+        #first the normal libraries
+        base_annotation = "$rootProject.ext."
+        ext_list = self.gradle_dict["ext"]
+        library_node = self.gradle_dict["dependencies"]["compile"]
+        library_extract = [ x for x in self.gradle_dict["dependencies"]["compile"] if len(x) == 1]
+        for lib in library_extract:
+            version_placeholder = ''
+            if base_annotation in lib[0]:
+                temp_lib = lib[0][:lib[0].index("$")]
+                version_placeholder = lib[0][lib[0].index("$"):]
+                required = version_placeholder.strip(base_annotation)
+                required_version = ext_list[required][0][0]
+                #TODO add check for versions
+                library_node.remove(lib)
+                library_node.append([temp_lib+required_version])
+
+    def checkElements(self, elements, target_list):
+        """
+        Checks whether any element in target_list exists in elements
+        @param elements: given element list, list or list of tuples
+        @param target_list:
+        @return:
+        """
+        elements_check = []
+        if type(elements[0]) == tuple:
+            elements_check = [val2 for (val1, val2) in elements]
+        else:
+            elements_check = elements
+        for target in target_list:
+            if target in elements_check:
+                return True
+        return False
+
+    def getElements(self, elements):
+        """
+        returns list of real elements without token tags
+        @param elements: list of tuples
+        @return: list of elements
+        """
+        if type(elements[0]) == tuple:
+            return [val2 for (val1, val2) in elements]
+        else:
+            return elements
+
+    def purifyElements(self, elements, filter_list):
+        """
+        removes unwanted elements from the list if they exist
+        @param elements: target element list
+        @param filter_list: elements to be filtered
+        @return: filtered list
+        """
+        el_lst = self.getElements(elements)
+        for el in filter_list:
+            if el in el_lst:
+                el_lst.remove(el)
+        return el_lst
